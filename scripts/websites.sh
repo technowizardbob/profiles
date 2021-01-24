1dbf2476759b3fe4e74b81ccd4a9bc546f406d79
Robert Strutts <bob_586@yahoo.com>
2021-01-23 22:16:19 -0500

    is dialog installed?

--- scripts/websites.sh
+++ scripts/websites.sh
@@ -3,6 +3,18 @@
 urls=/opt/profiles/sites/urls
 WEBSITE_BROWSER='firefox'
 
+dialog &> /dev/null || {
+    [[ $(uname -s) == "Darwin" ]] && \
+       echo -e "\nInstall dialog\nbrew install -y dialog"
+    if [[ -x /usr/bin/apt-get ]]; then
+       echo -e "\nInstall dialog\nsudo apt-get install -y dialog"
+    fi
+    if [[ -x /usr/bin/pacman ]]; then
+       echo -e "\nInstall dialog\nsudo pacman -S dialog"
+    fi
+    exit 1
+}
+
 refresh() {
    cmdlist=()
    IFSOLD=$IFS
