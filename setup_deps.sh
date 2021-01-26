d87b669430ce497d87773a1a09deaea0d0bfbea2
Robert Strutts <bob_586@yahoo.com>
2021-01-26 14:27:08 -0500

    setup

---
+++ setup_deps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ ! -r arch_deps.list ] || [ -r debian_deps.list ] \
+   [ ! -d /opt/profiles ]; then
+   echo "Please have this git repo in /opt/profiles"
+   echo "Then cd into it, \$ cd /opt/profiles"
+   exit 1
+fi
+
+if [ -x /bin/pacman ] || [ -x /usr/bin/pacman ]; then
+   sudo pacman -Syu
+   sudo pacman -S $(cat arch_deps.list) 
+elif [ -x /bin/apt-get ] || [ -x /usr/bin/apt-get ]; then
+   sudo apt-get update
+   sudo apt-get install -y $(cat debian_deps.list)
+fi
+
+if [ $? -eq 0 ]; then
+   echo -e "Success!\nNow run install.sh if you have not already..."
+fi
