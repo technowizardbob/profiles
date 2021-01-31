62aafc86c145d57716c78e489b4992fe8d32b08b
Robert Strutts <bob_586@yahoo.com>
2021-01-31 17:37:16 -0500

    shellcheck on pwdgen

--- scripts/pwd_gen.sh
+++ scripts/pwd_gen.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-PWD_SIZE=$((7 + $RANDOM % 16)) # Min 7, Max 7 + 16
+PWD_SIZE=$((7 + RANDOM % 16)) # Min 7, Max 7 + 16
 
 PCS='!@#%^&*()_-+=[].,;:?'
-SPC=${PCS:$(($RANDOM % ${#PCS})):1}   # Start Random special char.
-EPC=${PCS:$(($RANDOM % ${#PCS})):1}   # End Random special char.
+SPC=${PCS:$((RANDOM % ${#PCS})):1}   # Start Random special char.
+EPC=${PCS:$((RANDOM % ${#PCS})):1}   # End Random special char.
 
 export LC_ALL=C
 
 echo -e "$SPC\c"
-cat /dev/urandom | grep -ao '[A-Za-z0-9]' \
+    grep -ao '[A-Za-z0-9]' /dev/urandom \
     | head -n $PWD_SIZE \
     | shuf \
     | tr -d 'ioIOlL01\n'
-echo $EPC
+echo "$EPC"
