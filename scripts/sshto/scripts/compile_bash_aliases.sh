4b7abe5cdcb9c4b945d23c548b53489f5ef34c3f
Robert Strutts <bob_586@yahoo.com>
2021-02-22 16:19:28 -0500

    sshto copy bash aliases

--- scripts/sshto/scripts/compile_bash_aliases.sh
+++ scripts/sshto/scripts/compile_bash_aliases.sh
@@ -8,25 +8,25 @@ _ENV_FILES="${_ENV_PATH}*.env"
 
 renew_env_aliases() {
     ALLENVS=()
-    
+
     anyexists=$(ls -l $_CUSTOM_ENV_FILES 2> /dev/null | wc -l)
-        
+
     for eev in $_ENV_FILES; do
         skip=false
-        if [ ! $anyexists -eq 0 ]; then 
+        if [ ! $anyexists -eq 0 ]; then
            for cev in $_CUSTOM_ENV_FILES; do
               if [ $(basename $cev) == $(basename "$eev") ]; then
                  skip=true
                  break
-              fi   
+              fi
            done
-        fi   
+        fi
         if [ $skip = false ]; then
            ALLENVS+=( "$eev" )
         fi
     done
-    
-    if [ ! $anyexists -eq 0 ]; then 
+
+    if [ ! $anyexists -eq 0 ]; then
        for addce in $_CUSTOM_ENV_FILES; do
            ALLENVS+=( "$addce" )
        done
@@ -44,50 +44,61 @@ cat <<EOF >> ${_PROFILES_PATH}scripts/sshto/bin/.bash_aliases
      GIT_BRANCH_CHANGED_SYMBOL='+ changed'
      GIT_NEED_PULL_SYMBOL='⇣ do pull'
      GIT_NEED_PUSH_SYMBOL='⇡ needs push'
-    
+
      BOLD="\\[\$(tput bold)\\]"
      DIM="\\[\$(tput dim)\\]"
      RESET="\\[\$(tput sgr0)\\]"
      REVERSE="\\[\$(tput rev)\\]"
 
-        RED="\[\033[1;31m\]"
-     YELLOW="\[\033[1;33m\]"
-      GREEN="\[\033[0;32m\]"
-       BLUE="\[\033[1;34m\]"
-    MAGENTA="\[\033[1;35m\]"
-       CYAN="\[\033[1;36m\]"
-  LIGHT_RED="\[\033[1;31m\]"
-LIGHT_GREEN="\[\033[1;32m\]"
- LIGHT_BLUE="\[\033[1;94m\]"
- LIGHT_CYAN="\[\033[1;96m\]"
-      WHITE="\[\033[1;37m\]"
-      BLACK="\[\033[1;30m\]"
- LIGHT_GRAY="\[\033[1;37m\]"
- 
- BK_DEFAULT="\[\e[49m\]"
- BK_BLACK="\[\e[40m\]"
- BK_RED="\[\e[41m\]"
- BK_GREEN="\[\e[42m\]"
- BK_YELLOW="\[\e[43m\]"
- BK_BLUE="\[\e[44m\]"
- BK_LIGHT_RED="\[\e[101m\]"
- BK_LIGHT_GREEN="\[\e[102m\]"
- BK_LIGHT_YELLOW="\[\e[103m\]"
- BK_LIGHT_BLUE="\[\e[104m\]"
- BK_LIGHT_CYAN="\[\e[106m\]"
- BK_LIGHT_WHITE="\[\e[107m\]"
- BK_EXIT="\[\e[49m\]"
- 
- COLOR_NONE="\[\e[0m\]"
- COLOR_DEFAULT="\[\e[39m\]"
+        RED="\\[\\033[1;31m\\]"
+     YELLOW="\\[\\033[1;33m\\]"
+      GREEN="\\[\\033[0;32m\\]"
+       BLUE="\\[\\033[1;34m\\]"
+    MAGENTA="\\[\\033[1;35m\\]"
+       CYAN="\\[\\033[1;36m\\]"
+  LIGHT_RED="\\[\\033[1;31m\\]"
+LIGHT_GREEN="\\[\\033[1;32m\\]"
+ LIGHT_BLUE="\\[\\033[1;94m\\]"
+ LIGHT_CYAN="\\[\\033[1;96m\\]"
+      WHITE="\\[\\033[1;37m\\]"
+      BLACK="\\[\\033[1;30m\\]"
+ LIGHT_GRAY="\\[\\033[1;37m\\]"
+
+ BK_DEFAULT="\\[\\e[49m\\]"
+ BK_BLACK="\\[\\e[40m\\]"
+ BK_RED="\\[\\e[41m\\]"
+ BK_GREEN="\\[\\e[42m\\]"
+ BK_YELLOW="\\[\\e[43m\\]"
+ BK_BLUE="\\[\\e[44m\\]"
+ BK_LIGHT_RED="\\[\\e[101m\\]"
+ BK_LIGHT_GREEN="\\[\\e[102m\\]"
+ BK_LIGHT_YELLOW="\\[\\e[103m\\]"
+ BK_LIGHT_BLUE="\\[\\e[104m\\]"
+ BK_LIGHT_CYAN="\\[\\e[106m\\]"
+ BK_LIGHT_WHITE="\\[\\e[107m\\]"
+ BK_EXIT="\\[\\e[49m\\]"
+
+ COLOR_NONE="\\[\\e[0m\\]"
+ COLOR_DEFAULT="\\[\\e[39m\\]"
 
 function is_normal_theme() {
+    if [ -f ~/.unicode_support ]; then
      PS_SYMBOL='🐧'
-     SEGMENT_SEPARATOR=\$'\ue0b0'
-     PL_BRANCH_CHAR=\$'\ue0a0'         # 
-     return 0
+     SEGMENT_SEPARATOR=\$'\\ue0b0'
+     PL_BRANCH_CHAR=\$'\\ue0a0'         # 
+    else
+     PS_SYMBOL=''
+     SEGMENT_SEPARATOR=' -> '
+     PL_BRANCH_CHAR=''
+    fi
+
+    if [ ! -f ~/.use_theme ]; then
+        return 1
+    else
+        return 0
+    fi
 }
- 
+
 __git_info() {
         [ -d .git ] || return
 
@@ -100,16 +111,16 @@ __git_info() {
         branch="\$(git symbolic-ref --short HEAD 2>/dev/null || git describe --tags --always 2>/dev/null)"
         [ -n "\$branch" ] || return  # git branch not found
 
-        stats="\$(git status --porcelain --branch | grep '^##' | grep -o '\[.\+\]\$')"
-        aheadN="\$(echo "\$stats" | grep -o 'ahead \d\+' | grep -o '\d\+')"
-        behindN="\$(echo "\$stats" | grep -o 'behind \d\+' | grep -o '\d\+')"
+        stats="\$(git status --porcelain --branch | grep '^##' | grep -o '\\[.\\+\\]\$')"
+        aheadN="\$(echo "\$stats" | grep -o 'ahead \\d\\+' | grep -o '\\d\\+')"
+        behindN="\$(echo "\$stats" | grep -o 'behind \\d\\+' | grep -o '\\d\\+')"
         [ -n "\$aheadN" ] && marks+=" \$GIT_NEED_PUSH_SYMBOL\$aheadN"
         [ -n "\$behindN" ] && marks+=" \$GIT_NEED_PULL_SYMBOL\$behindN"
 
         if ! is_normal_theme; then
             printf "%s" "\$state[\$branch]\$WHITE\$marks"
-           return 
-        fi   
+           return
+        fi
 
         # print the git branch segment without a trailing newline
         # branch is modified?
@@ -118,16 +129,16 @@ __git_info() {
         else
             printf "%s" "\$RESET\$bk\$WHITE[\$branch]\$marks\$RESET\$state"
         fi
-    } 
- 
+    }
+
 function is_git_repository {
   git branch > /dev/null 2>&1
 }
- 
+
 function set_git_branch {
   # Capture the output of the "git status" command.
   git_status="\$(git status 2> /dev/null)"
- 
+
   # Set color based on clean/staged/dirty.
   if [[ \${git_status} =~ "working tree clean" ]]; then
     state="\${GREEN}"
@@ -139,7 +150,7 @@ function set_git_branch {
     state="\${RED}"
     bk="\$BK_RED"
   fi
-  
+
   # Set arrow icon based on status against remote.
   remote_pattern="# Your branch is (.*) of"
   if [[ \${git_status} =~ \${remote_pattern} ]]; then
@@ -155,27 +166,27 @@ function set_git_branch {
   if [[ \${git_status} =~ \${diverge_pattern} ]]; then
     remote="↕ changed"
   fi
-  
+
   BRANCH="\${state}(git)\${COLOR_NONE}\${remote} "
   BRANCH+="\$(__git_info bk)"
   if is_normal_theme; then
     BRANCH+="\$BK_EXIT\${SEGMENT_SEPARATOR}\$RESET"
-  fi    
+  fi
 }
 
 function set_prompt_symbol() {
   if is_normal_theme; then
      ERRX="✘"
-  else 
+  else
      ERRX="Opps!"
-  fi    
+  fi
   if test \$1 -eq 0 ; then
       PROMPT_SYMBOL="\\\$\${COLOR_NONE}"
   else
       PROMPT_SYMBOL="\${RED}\${ERRX} \\\$\${COLOR_NONE}"
   fi
 }
- 
+
 # Set the full bash prompt.
 function set_bash_prompt() {
   set_prompt_symbol \$?
@@ -185,10 +196,10 @@ function set_bash_prompt() {
     BRANCH=''
   fi
   if [ -n "\$SSH_CLIENT" ] || [ -n "\$SSH_TTY" ]; then
-    UPC="${RED}SSH:${WHITE}"
+    UPC="\${RED}SSH:\${WHITE}"
   else
-    UPC="Local:"    
-  fi 
+    UPC="Local:"
+  fi
 EOF
 
 if [ -f "${_PROFILES_PATH}theme" ]; then
