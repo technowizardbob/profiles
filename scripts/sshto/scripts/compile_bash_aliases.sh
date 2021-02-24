196ef5e27d3e14b8f6c18e121b873586cbb63191
Robert Strutts <bob_586@yahoo.com>
2021-02-23 20:21:42 -0500

    + todo and pwdgen

--- scripts/sshto/scripts/compile_bash_aliases.sh
+++ scripts/sshto/scripts/compile_bash_aliases.sh
@@ -3,5 +3,5 @@
 _PROFILES_PATH="/opt/profiles"
 
 pushd ${_PROFILES_PATH}
-tar -zvcf scripts/sshto/bin/bash_aliases.tar.gz .bashrc .bash_aliases .git_bash_prompt theme aliases/ cheats/ custom_aliases/
+tar -zvcf scripts/sshto/bin/bash_aliases.tar.gz .bashrc .bash_aliases .git_bash_prompt theme aliases/ cheats/ custom_aliases/ scripts/pwd_gen.sh scripts/todo/
 popd
