52049ffee63108dc110c508b6de8df5bd88142e5
Robert Strutts <bob_586@yahoo.com>
2021-02-24 15:30:04 -0500

    mempass

--- scripts/pwd_gen.sh
+++ scripts/pwd_gen.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-PWD_SIZE=$((7 + RANDOM % 16)) # Min 7, Max 7 + 16
+PWD_SIZE=$((9 + RANDOM % 16)) # Min 9, Max 9 + 16
 
-PCS='!@#%^&*()_-+=[].,;:?'
+PCS='@#%^&*()_-+=[].,;:?'
 SPC=${PCS:$((RANDOM % ${#PCS})):1}   # Start Random special char.
 EPC=${PCS:$((RANDOM % ${#PCS})):1}   # End Random special char.
 
 export LC_ALL=C
 
-echo -e "$SPC\c"
-    grep -ao '[A-Za-z0-9]' /dev/urandom \
+tpwd=$(grep -ao '[A-Za-z0-9]' /dev/urandom \
     | head -n $PWD_SIZE \
     | shuf \
-    | tr -d 'ioIOlL01\n'
-echo "$EPC"
+    | tr -d 'ioIOlL01\n')
+echo "${SPC}${tpwd}${EPC}" | fold -w1 | shuf | tr -d '\n'
+echo -e "\n"
