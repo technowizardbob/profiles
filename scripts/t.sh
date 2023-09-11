ee7198d666a85b991c9efc2feb5fd1977986b5ee
Robert <Robert@TryingToScale.com>
2023-09-11 17:45:26 -0400

    re-t

--- scripts/t.sh
+++ scripts/t.sh
@@ -5,25 +5,16 @@ if [ ! -d ~/.tmux/plugins/tpm ]; then
 fi
 
 # Create a new session named "myCode"
-tmux new-session -d -s myCode
+tmux new-session -d -s myCode -n main
+tmux new-window -t myCode -d -n ssh
 
 # Split the window vertically
-tmux split-window -v
+tmux split-window -v -t myCode
 
-# Split the first pane horizontally
-# tmux split-pane -h -t myCode:1.1
+tmux send-keys -t myCode:main.2 'gpcd' C-m
+tmux send-keys -t myCode:ssh 'sshto' C-m
 
-#tmux send-keys -t myCode:1.1 'clear' C-m
-tmux send-keys -t myCode:1.2 'gpcd' C-m
-tmux select-pane -t myCode:1.1
-
-tmux rename-window -t 1 "main"
-tmux new-window -n "ssh"
-
-# Window 2, Panel 1, type/run sshto
-tmux send-keys -t myCode:2.1 'sshto' C-m
-# Back to main Window
-tmux select-window -t 1
+tmux select-pane -t myCode:main.1
 
 # Attach to the session
 tmux attach-session -t myCode
