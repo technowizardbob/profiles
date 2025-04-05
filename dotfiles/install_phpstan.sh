83bb1ba15899adf4c59d72b9bd4816c9b66f900f
Robert <Robert@TryingToScale.com>
2025-04-05 17:45:20 -0400

    phpstan as phx

---
+++ dotfiles/install_phpstan.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ -x /usr/local/bin/phpx ]]; then
+	echo "Already Installed!"
+else
+	mkdir -p /tmp/stan
+	pushd /tmp/stan
+	cat <<EOF >> phpx
+#!/usr/bin/env php
+<?php
+
+declare (strict_types=1);
+
+Phar::loadPhar(__DIR__ . '/phpx.phar', 'phpstan.phar');
+
+require 'phar://phpstan.phar/bin/phpstan';
+EOF
+    chmod +x phpx
+    sudo mv phpx /usr/local/bin/
+	wget https://github.com/phpstan/phpstan/releases/latest/download/phpstan.phar
+	mv phpstan.phar phpx.phar
+	chmod +x phpx.phar
+	sudo mv phpx.phar /usr/local/bin/
+	popd
+	rmdir /tmp/stan
+fi
